ee54d6ac2f187ede19c57d5762ba2d32e7a09675
Ryan Cole <admin@ryanc.me>
2022-04-22 16:53:49 +1200

    AoC2018-01: Complete part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,18 +1,37 @@
 use std::error::Error;
 use std::fs::File;
-use std::io::{BufReader, prelude::*};
+use std::io::{BufReader, Read, prelude::*};
+use std::collections::HashSet;
 
 const FREQUENCIES_FILE: &str = "./input/frequencies.txt";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let freqs = BufReader::new(File::open(&FREQUENCIES_FILE)?);
-    let result: i32 = freqs
-        .lines()
+    let freqs_buf = BufReader::new(File::open(&FREQUENCIES_FILE)?);
+    let freqs: Vec<i32> = freqs_buf.lines()
         .filter_map(|x| x.ok())
         .map(|x| x.parse::<i32>().ok())
         .flatten()
-        .sum();
+        .collect();
 
-    println!("{:?}", result);
+    // part 1
+    let total: i32 = freqs.iter().sum();
+    println!("Total: {:?}", total);
+    
+    // part 2
+    let mut seen: HashSet<i32> = HashSet::new();
+    let mut freq: i32 = 0;
+    let mut calibration: Option<i32> = None;
+    for x in freqs.iter().cycle() {
+        freq = freq + x;
+        if seen.contains(&freq) {
+            calibration = Some(freq);
+            break
+        }
+        else {
+            seen.insert(freq);
+        }
+    }
+    println!("Calibration: {:?}", calibration);
+ 
     Ok(())
 }
